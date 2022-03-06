f95dfe692abb4be11d64d9841ff7e3c86faa3e07
Adilet24 <96615575+Adilet24@users.noreply.github.com>
2022-03-07 02:52:45 +0600

    Add files via upload

---
+++ JS/about.js
@@ -0,0 +1,23 @@
+function onmouseoverNavPrice(){
+  document.getElementById("price").style.fontWeight = "bold";
+}
+
+function onmouseoutNavPrice(){
+  document.getElementById("price").style.fontWeight = "normal";
+}
+
+function onmouseoverNavAbout(){
+  document.getElementById("about").style.fontWeight = "bold";
+}
+
+function onmouseoutNavAbout(){
+  document.getElementById("about").style.fontWeight = "normal";
+}
+
+function onmouseoverNavRegister(){
+  document.getElementById("register").style.fontWeight = "bold";
+}
+
+function onmouseoutNavRegister(){
+  document.getElementById("register").style.fontWeight = "normal";
+}
---
+++ JS/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("#sale").text("Only until 28.02. From 5825 tenge/month");
+
+});
+
+function membershipF(){
+  var phone = prompt("Please write your number here for consultation");
+}
+
+function onmouseoverNavPrice(){
+  document.getElementById("price").style.fontWeight = "bold";
+}
+
+function onmouseoutNavPrice(){
+  document.getElementById("price").style.fontWeight = "normal";
+}
+
+function onmouseoverNavAbout(){
+  document.getElementById("about").style.fontWeight = "bold";
+}
+
+function onmouseoutNavAbout(){
+  document.getElementById("about").style.fontWeight = "normal";
+}
+
+function onmouseoverNavRegister(){
+  document.getElementById("register").style.fontWeight = "bold";
+}
+
+function onmouseoutNavRegister(){
+  document.getElementById("register").style.fontWeight = "normal";
+}
